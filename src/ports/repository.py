becdf5b890957ba4821680f68fada94d8b56a00b
Mario <team.miningzone@gmail.com>
2025-05-31 17:08:31 +0200

    feat(ports): add PackageRepository interface/port

---
+++ src/ports/repository.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from src.domain import Package
+
+class PackageRepository(ABC):
+    """
+    Interface (port) for the package repository.
+    Any implementation (in-memory, database-based, etc.)
+    must inherit from this class and provide the methods declared here.
+    """
+
+    @abstractmethod
+    async def get_by_id(self, package_id: str) -> Package:
+        """
+        Gets a Package from its ID.
+        Must throw a PackageNotFoundError exception if it doesn't exist.
+        """
+        ...
+
+    @abstractmethod
+    async def save(self, package: Package) -> None:
+        """
+        Persists or updates a Package in storage.
+        """
+        ...
