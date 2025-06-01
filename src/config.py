0e10f2ee1b04615b8607e4df7600074c71315f24
Mario <team.miningzone@gmail.com>
2025-06-01 19:48:59 +0200

    feat(config): added more sample cases

--- src/config.py
+++ src/config.py
@@ -3,8 +3,10 @@ from typing import List
 class Settings:
     app_name: str = "Paack Assignment API"
     preload_addresses: List[str] = [
-        "Calle Prueba 123, Madrid",
-        "Avenida Siempre Viva 456, Barcelona"
+        "Calle Prueba 123 4 3A, Madrid",
+        "Avenida Siempre Viva 456 4C, Barcelona",
+        "Calle del Naranjo 10 Bajo B, Valencia",
+        "Calle de la Zamburiña 124 5A, Galicia"
     ]
 
 settings = Settings()
