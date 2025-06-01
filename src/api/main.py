cbff37cb1b2c12b950349116874da0bbee019066
Mario <team.miningzone@gmail.com>
2025-06-01 19:43:30 +0200

    feat(api): Modifications needed to list and update the status of packages

--- src/api/main.py
+++ src/api/main.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI
-from src.api.routers import router
+from contextlib import asynccontextmanager
+from src.api.routers import router, repository  
 from src.config import settings
+from src.domain.entities import Package
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Lifespan manager. Runs before the server boots and allows for initialization tasks (preload).
+    """
+    # Startup: precharge memory packages
+    packages_to_preload = [
+        Package(customer_address=addr)
+        for addr in settings.preload_addresses
+    ]
+
+    await repository.preload_packages(packages_to_preload)
+    yield
 
 app = FastAPI(
     title=settings.app_name,
-    version="1.0.0"
+    version="1.0.0",
+    lifespan=lifespan
 )
 
 app.include_router(router)
-
 
 @app.get("/health", status_code=200)
 async def healthcheck():
--- src/api/routers.py
+++ src/api/routers.py
@@ -1,11 +1,60 @@
-from fastapi import APIRouter
+from typing import List
+from fastapi import APIRouter, HTTPException, status, BackgroundTasks
+from src.domain.exceptions import PackageNotFoundError, InvalidStateTransitionError
+from src.adapters.repository.in_memory_repository import InMemoryPackageRepository
+from src.adapters.notification.notification_stub import NotificationStub
+from src.use_cases.update_package_status import UpdatePackageStatusUseCase
+from src.api.schemas import PackageStatusUpdateRequest, PackageResponse
 
 router = APIRouter()
 
-@router.get("/ping")
-async def ping():
+repository = InMemoryPackageRepository()
+notification_adapter = NotificationStub()
+use_case = UpdatePackageStatusUseCase(repository)
+
+@router.patch(
+    "/packages/{package_id}/status",
+    response_model=PackageResponse,
+    status_code=status.HTTP_200_OK
+)
+async def update_package_status(package_id: str, body: PackageStatusUpdateRequest,
+                                background_tasks: BackgroundTasks):
     """
-    Test endpoint to verify that the API is running correctly.
-    Calling GET /ping will return {"message": "pong"}.
+    Updates the status of a package given its ID.
     """
-    return {"message": "pong"}
+    try:
+        updated_package = await use_case.execute(package_id, body.status)
+
+        background_tasks.add_task(
+            notification_adapter.notify_status_changed,
+            updated_package
+        )
+
+        return updated_package
+    
+    except PackageNotFoundError as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(e)
+        )
+
+    except InvalidStateTransitionError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e)
+        )
+    
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=e
+        )
+    
+@router.get(
+    "/packages",
+    response_model=List[PackageResponse],
+    status_code=status.HTTP_200_OK
+)
+async def list_packages():
+    all_packages = await repository.list_all()
+    return all_packages
